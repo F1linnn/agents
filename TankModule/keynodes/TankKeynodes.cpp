eb62cd43911b4f2031832ab2485bd493b990b757
Аляксандр <73547252+F1linnn@users.noreply.github.com>
2022-12-05 17:57:57 +0300

    Add files via upload

---
+++ TankModule/keynodes/TankKeynodes.cpp
@@ -0,0 +1,13 @@
+/*
+ * Author Soluth
+ */
+
+#include "sc-memory/sc_memory.hpp"
+
+#include "TankKeynodes.hpp"
+
+namespace tankModule
+{
+ScAddr TankKeynodes::nrel_parameters;
+ScAddr TankKeynodes::action_TankSearch;
+}  // namespace tankModule
